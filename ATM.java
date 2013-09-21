7e79ae84620802f96678ae4a1dd69cbf6b5eb842
Soufii <abbshr@outlook.com>
2013-09-21 17:03:09 +0800

    init2

--- ATM.java
+++ ATM.java
@@ -3,60 +3,60 @@ import java.util.*;
 import java.text.*;
 
 public class ATM  {
-	public static void main(String[] args) {
-		Scanner infos = new Scanner(new File("/home/soufii/userInfo"));
+	public static void main(String[] args)  throws IOException { 
+		Scanner infos = new Scanner(new File("/home/soufii/JavaExp/userInfo"));
 		String rec = infos.nextLine();
 		int i = 0, num = 0;
 		while (infos.hasNextLine()) {
 			i++;
 			infos.nextLine();
 		}
-		String[] userArr = new String[i];
+System.out.print(i);
+		String[] userArr = new String[i + 1];
 		i = 0;
-		Scanner infos1 = new Scanner(new File("/home/soufii/userInfo"));
-		rec = infos1.nextLine();
+		Scanner infos1 = new Scanner(new File("/home/soufii/JavaExp/userInfo"));
 		while (infos1.hasNextLine()) {
 			userArr[i] = infos1.nextLine();
 			i++;
-			infos1.nextLine();
 		}
 		Boolean isAdmin = false;
 		Boolean isLogin = false;
 		Scanner in = new Scanner(System.in);
-		Scanner userInfo = new Scanner(new File("/home/soufii/userInfo"));
+		Scanner userInfo = new Scanner(new File("/home/soufii/JavaExp/userInfo"));
 		System.out.print("请输入卡号：");
 		String id = in.nextLine();
 		System.out.print("请输入密码：");
 		String pwd = in.nextLine();
 		int money;
-		String time, type;
-		String line = userInfo.nextLine();
-		
+		String time, type, line;
 		while (userInfo.hasNextLine()) {
+			line = userInfo.nextLine();
 			StringTokenizer tokens = new StringTokenizer(line,"#");
-			if (tokens.nextToken() == id && tokens.nextToken() == pwd) {
+			String inpid = tokens.nextToken();
+			String inppwd = tokens.nextToken();
+			if (inpid .equals( id) && inppwd .equals( pwd)) {
 				System.out.print("登陆成功！");
 				money = Integer.parseInt(tokens.nextToken());
 				type = tokens.nextToken();
 				for (i = 0; i < userArr.length; i++) {
-					if (userArr[i] ==line) 
+					if (userArr[i] .equals(line)) 
 						num = i;
 				}
-				if (tokens.nextToken() == "admin") {
-					System.out.print("欢迎，管理员~\n");
+				if (type.equals( "admin")) {
+					System.out.print("欢迎，管理员\n");
 					isAdmin = true;
 				}
 				time = tokens.nextToken();
-				showMenu(isAdmin, id, pwd, money, type, time, userArr, num);
+				System.out.print(num);
 
+				showMenu(isAdmin, id, pwd, money, type, time, userArr, num);
 				isLogin = true;
 				break;
 			}
-			line = userInfo.nextLine();
 		}
 		if (!isLogin) System.out.print("用户卡号或密码有误！");
 	}
-	public static void showMenu(Boolean isAdmin, String id, String pwd, int money, String type, String time, String[] userArr, int num) {
+	public static void showMenu(Boolean isAdmin, String id, String pwd, int money, String type, String time, String[] userArr, int num) throws IOException {
 		if (isAdmin) {
 			showAdminMenu(userArr, num, id, pwd, time);
 		}
@@ -65,7 +65,7 @@ public class ATM  {
 			showUserMenu(user, userArr, num);
 		}
 	}
-	public static void showUserMenu(User user, String[] userArr, int num) {
+	public static void showUserMenu(User user, String[] userArr, int num)  throws IOException{
 		Boolean isLoop = true;
 		while (isLoop) {
 			System.out.print("1. 查询余额\n");
@@ -87,57 +87,59 @@ public class ATM  {
 					n = in.nextInt();
 					user.save(n);
 					nmoney = Integer.toString(user.getMoney());
-					userArr[num] = user.id + "#" + user.pwd + "#" + nmoney + "#" + "hq" + user.time + "\n";
+					userArr[num] = user.id + "#" + user.pwd + "#" + Integer.toString(user.money) + "#" + "hq" + "#" + user.time + "\n";
 					break;
 				case 3:
 					System.out.print("取多少?");
 					n= in.nextInt();
 					user.pick(n);
 					nmoney = Integer.toString(user.getMoney());
-					userArr[num] = user.id + "#" + user.pwd + "#" + nmoney + "#" + "hq" + user.time + "\n";
+					userArr[num] = user.id + "#" + user.pwd + "#" + Integer.toString(user.money) + "#" + "hq" + "#" + user.time + "\n";
 					break;
 				case 4:
 					User.getInfo(user.getId());
+					break;
 				case 5:
 					System.out.print("旧密码:");
-					String opwd = in.nextLine();
-					if (opwd == user.pwd) {
-						String npwd = in.nextLine();
+					Scanner inin = new Scanner(System.in);
+					String opwd = inin.nextLine();
+					if (opwd.equals( user.pwd)) {
+						System.out.print("新密码:");
+						String npwd = inin.nextLine();
 						if (npwd.length() >= 6 && Integer.parseInt(npwd) % 111111 != 0) {
 							user.chgPwd(npwd);
-							userArr[num] = user.id + "#" + npwd + "#" + nmoney + '#' + "hq" + user.time + "\n";
+							userArr[num] = user.id + "#" + npwd + "#" + user.money + '#' + "hq" + "#" + user.time + "\n";
 						}
 					}
 
 					break;
 				case 0:
 					isLoop = false;
-					FileWriter writer = new FileWriter("/home/soufii/userInfo");
+					FileWriter writer = new FileWriter("/home/soufii/JavaExp/userInfo");
 					for (int i = 0; i < userArr.length; i++) {
-						writer.write(userArr[i]);
+						writer.write(userArr[i] + "\n");
 						writer.flush();
 					}
 					
 					writer.close();
 					break;
 			}
-
 		}
 	}
-	public static void showAdminMenu(String[] userArr, int num, String id, String pwd, String time) {
-		Scanner userInfo = new Scanner(new File("/home/soufii/userInfo"));
-		String line = userInfo.nextLine();
+	public static void showAdminMenu(String[] userArr, int num, String id, String pwd, String time) throws IOException {
+		String line;
 		int money = 0;
+		Scanner userInfo = new Scanner(new File("/home/soufii/JavaExp/userInfo"));
 		while (userInfo.hasNextLine()) {
+			line = userInfo.nextLine();
 			StringTokenizer tokens = new StringTokenizer(line,"#");
 			tokens.nextToken();
 			tokens.nextToken();
 			money += Integer.parseInt(tokens.nextToken());
-			line = userInfo.nextLine();
 		}
 		User.allMoney = money;
 		Boolean isLoop = true;
-		FileWriter writer = new FileWriter("/home/soufii/userInfo");
+		FileWriter writer = new FileWriter("/home/soufii/JavaExp/userInfo");
 		while (isLoop) {
 			System.out.print("1. 所有用户信息\n");
 			System.out.print("2. 创建新号\n");
@@ -149,18 +151,20 @@ public class ATM  {
 			String npwd;
 			switch (choose) {
 				case 1:
-					Scanner allInfo = new Scanner(new File("/home/soufii/userInfo"));
-					String aline = allInfo.nextLine();
+					Scanner allInfo = new Scanner(new File("/home/soufii/JavaExp/userInfo"));
+					String aline;
 					while (allInfo.hasNextLine()) {
-						System.out.print(aline);
 						aline = allInfo.nextLine();
+						System.out.print(aline + "\n");
 					}
 					break;
 				case 2:
 					Date now = new Date(); 
 					System.out.print("创建新号");
-					String nid = in.nextLine();
-					npwd = in.nextLine();
+					Scanner inin = new Scanner(System.in);
+					String nid = inin.nextLine();
+					System.out.print("密码");
+					npwd = inin.nextLine();
 					DateFormat stamp = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM); 
       				String ntime = stamp.format(now);
 					User newUser = new User(nid, npwd, 10000, "hq", ntime);
@@ -168,24 +172,25 @@ public class ATM  {
 					for (int i = 0; i < userArr.length; i++) {
 						nuserArr[i] = userArr[i];
 					}
-					nuserArr[userArr.length] = nid + "#" + npwd + "#" + '0' + "#" + "hq" + ntime + "\n";
+					nuserArr[userArr.length] = nid + "#" + npwd + "#" + "10000" + "#" + "hq" + "#" + ntime + "\n";
 					User.saveToATM(10000);
 					for (int i = 0; i < nuserArr.length; i++) {
-						writer.write(nuserArr[i]);
-						writer.flush();
+						writer.write(nuserArr[i] + "\n");
+						System.out.print(nuserArr[i]);
 					}
+					writer.flush();
 					break;
 				case 3:
-					System.out.print("");
-					User.getAllMoney();
+					System.out.print(User.getAllMoney());
 					break;
 				case 4:
 					System.out.print("旧密码:");
 					String opwd = in.nextLine();
-					if (opwd == pwd) {
+					if (opwd .equals( pwd)) {
+						System.out.print("新密码");
 						npwd = in.nextLine();
 						if (npwd.length() >= 6 && Integer.parseInt(npwd) % 111111 != 0) {
-							userArr[num] = id + "#" + npwd + "#" + '0' + "#" + "admin" + time + "\n";
+							userArr[num] = id + "#" + npwd + "#" + '0' + "#" + "admin" + "#" + time + "\n";
 						}
 					}
 					for (int i = 0; i < userArr.length; i++) {
@@ -204,26 +209,27 @@ public class ATM  {
 }
 
 class User {
-	public User(String id, String pwd, int money, String type, String time) {
+	public User(String id, String pwd, int money, String type, String time) throws IOException {
 		this.id = id;
 		this.pwd = pwd;
 		this.money = money;
 		this.type = type;
 		this.time = time;
 	}
-	public int getMoney() {
+	public int getMoney() throws IOException {
 		return this.money;
 	}
-	public void save(int money) {
+	public void save(int money) throws IOException {
 		this.money += money;
 		allMoney += money;
+System.out.print(this.money);
 		Record record = new Record(id);
 		Date now = new Date();
 		DateFormat stamp = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM); 
       	String time = stamp.format(now);
 		record.save(id, money, time);
 	}
-	public void pick(int money) {
+	public void pick(int money) throws IOException {
 		this.money -= money;
 		allMoney -= money;
 		Record record = new Record(id);
@@ -232,20 +238,20 @@ class User {
       	String time = stamp.format(now);
 		record.save(id, -money, time);
 	}
-	public void chgPwd(String pwd) {
+	public void chgPwd(String pwd) throws IOException {
 		this.pwd = pwd;
 	}
 	public String getId() {
 		return this.id;
 	}
-	public static void getInfo(String id) {
+	public static void getInfo(String id) throws IOException {
 		Record record = new Record(id);
 		record.out();
 	}
-	public static int getAllMoney() {
+	public static int getAllMoney()throws IOException {
 		return allMoney;
 	}
-	public static void saveToATM(int money) {
+	public static void saveToATM(int money) throws IOException{
 		allMoney += money;
 	}
 	public String id,
@@ -257,22 +263,22 @@ class User {
 }
 
 class Record {
-	public Record(String id) {
+	public Record(String id) throws IOException{
 		this.id = id;
 	}
-	public void out() {
-		Scanner info = new Scanner(new File("/home/soufii/record"));
-		String line = info.nextLine();
+	public void out() throws IOException {
+		Scanner info = new Scanner(new File("/home/soufii/JavaExp/record"));
+		String line;
 		while (info.hasNextLine()) {
+			line = info.nextLine();
 			StringTokenizer tokens = new StringTokenizer(line,"#");
-			if (id == tokens.nextToken()) {
+			if (id .equals( tokens.nextToken())) {
 				System.out.print(line);
 			}
-			line = info.nextLine();
 		}
 	}
-	public void save(String id, int opt, String time) {
-		FileWriter writer = new FileWriter("/home/soufii/record", true);  
+	public void save(String id, int opt, String time) throws IOException {
+		FileWriter writer = new FileWriter("/home/soufii/JavaExp/record", true);  
 		String s = Integer.toString(opt); 
 		writer.write(id + "#" + s + "#" + time + "\n");
 		writer.flush();  
